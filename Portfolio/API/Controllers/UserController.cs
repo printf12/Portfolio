67f024969f9710e109ba9ed2602130e2c369fdfb
Oussema Acheche <oacheche@kpmg.com>
2020-12-29 09:22:33 +0100

    Add UserController for Test Reason also update database to have new user in table user

---
+++ Portfolio/API/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Controllers;
+using System.Collections;
+using API.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = _context.User.ToList();
+            return await Task.FromResult(users);
+        }
+        // api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserAsync(int id)
+        {
+            return await _context.User.FindAsync(id);
+        }
+    }
+}
