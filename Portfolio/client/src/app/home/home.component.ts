a0e8b27209874917204423920e05d350ae0154fc
Oussema Acheche <oacheche@kpmg.com>
2020-12-29 12:13:03 +0100

    Displaying fetched data

--- Portfolio/client/src/app/home/home.component.ts
+++ Portfolio/client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: any;
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get("https://localhost:44376/api/users").subscribe(response =>{
+      this.users = response;
+      }, error =>{
+        console.error();
+    })
   }
 
 }
